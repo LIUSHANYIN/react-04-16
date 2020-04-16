81323439051665b78e1ffdde15bb0f6a8a589fd9
shanyin liu <liu.shany@husky.neu.edu>
2020-04-16 17:55:22 -0400

    first

---
+++ precache-manifest.4c74922e62eb8b9d0ee4d80adbe50b08.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dc955e41f2eb30b8e457ef3b23926a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d26cc5a03ce9c208b783",
+    "url": "/static/css/main.ccef4865.chunk.css"
+  },
+  {
+    "revision": "b52faf6725c242775b0a",
+    "url": "/static/js/2.428a6510.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.428a6510.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d26cc5a03ce9c208b783",
+    "url": "/static/js/main.916e5dbd.chunk.js"
+  },
+  {
+    "revision": "9d9ac827ebb398718228",
+    "url": "/static/js/runtime-main.5d19c65d.js"
+  },
+  {
+    "revision": "9d1d6ea5176839ce8fdd479eedd1b033",
+    "url": "/static/media/Cold War.9d1d6ea5.png"
+  },
+  {
+    "revision": "b00696af645ba09f26e19b3354745c8b",
+    "url": "/static/media/Description.b00696af.png"
+  },
+  {
+    "revision": "06c1162e0c710dea617212e159324823",
+    "url": "/static/media/Head.06c1162e.png"
+  },
+  {
+    "revision": "84dd168fabadcaf29ed2491ea4ca5d69",
+    "url": "/static/media/Ida.84dd168f.png"
+  },
+  {
+    "revision": "8cae7f4dd1334dde800b6f07ac5b8363",
+    "url": "/static/media/Ils aimaient la vie.8cae7f4d.png"
+  },
+  {
+    "revision": "917309a763403e4d4abe5f9b14374c1e",
+    "url": "/static/media/bg-poland@2x.917309a7.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
